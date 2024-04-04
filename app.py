fcf2e829ff0656acbe90fa34c23b2be1e814d572
santiagodonoso <santiagodonoso@gmail.com>
2024-04-04 18:33:22 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from bottle import default_app, get, post
+import git 
+
+# https://GITHUB-TOKEN-HERE@github.com/GITUHB-USERNAME/mysite.git
+
+##############################
+@post('/SECRET-URL-HERE')
+def git_update():
+  repo = git.Repo('./mixroof')
+  origin = repo.remotes.origin
+  repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return ""
+
+##############################
+@get("/")
+def _():
+    return "x"
+
+##############################
+app = default_app()

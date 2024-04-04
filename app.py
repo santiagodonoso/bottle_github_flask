a1d02548845c188a7a17291e0b355837e5937a52
santiagodonoso <santiagodonoso@gmail.com>
2024-04-04 18:56:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import git
 ##############################
 @post('/SECRET-URL-HERE')
 def git_update():
-  repo = git.Repo('./mixroof')
+  repo = git.Repo('./mysite')
   origin = repo.remotes.origin
   repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
   origin.pull()
